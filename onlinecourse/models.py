84d008a9a95296b62199f619a3078e3ce602d960
Riccardo Abballe <riccardo.abballe@gmail.com>
2023-07-11 08:08:54 -0400

    models fix

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -103,12 +103,11 @@ class Enrollment(models.Model):
     # Other fields and methods you would like to design
 class Question(models.Model):
     # Foreign key to lesson
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     # question text
     question_text = models.TextField()
     # question grade/mark
-    question_grade = models.IntegerField(default=0)
-    course = models.ManyToManyField(Course)
+    question_grade = models.IntegerField(default=0))
 
     # <HINT> A sample model method to calculate if learner get the score of the question
     def is_get_score(self, selected_ids):
@@ -127,10 +126,9 @@ class Question(models.Model):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 class Choice(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    questions = models.ManyToManyField(Question)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_content = models.TextField()
-    is_correct = False
+    is_correct = models.BooleanField(default = False)
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
